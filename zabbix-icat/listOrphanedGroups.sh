41eb0d442d91a6661b256b70fe73c107b6b53fe3
Sietse Snel <s.t.snel@uu.nl>
2025-12-16 14:34:18 +0100

    Remove unused checks and update templates
    We have decided to remove most application-specific checks,
    since these are used little in practice. The iRODS log checks
    and OOM event check will remain in use for the time being.
    
    Also update templates for Zabbix 7.x

--- zabbix-icat/listOrphanedGroups.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# \file     listOrphanedGroups.sh
-# \brief    Call rule to return a list of groups without an internal user (...@uu.nl)
-# \author    Niek Bats
-# \copyright    Copyright (c) 2018, Utrecht University. All rights reserved.
-
-sudo -u irods irule -r irods_rule_engine_plugin-irods_rule_language-instance -F /etc/zabbix/zabbix_agentd.d/listOrphanedGroups.r
